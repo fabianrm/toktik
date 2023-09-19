07c6276268c49724beced7d80673320ff3b985bf
Fabian Ramirez <fabianrm@gmail.com>
2023-09-19 18:47:28 -0500

    Inicio implementacion repo x api

--- lib/shared/data/local_video_posts.dart
+++ lib/shared/data/local_video_posts.dart
@@ -3,7 +3,7 @@ List<Map<String, dynamic>> videoPosts = [
     'name': 'Subiendo escaleras automáticas',
     'videoUrl': 'assets/videos/1.mp4',
     'likes': 23230,
-    'views': 1523,
+    'views': 1020,
   },
   {
     'name': 'Planta apreciada por peatones',
