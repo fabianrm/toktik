25b15b6ea6fcb991bc1fb06105918e6b078ab0e4
Fabian Ramirez <fabianrm@gmail.com>
2023-09-20 09:29:45 -0500

    implementacion de datasource y repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infraestructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infraestructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover_screen.dart';
 
@@ -9,10 +11,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
+    final videoPostRepository =
+        VideoPostsRepositoryImpl(videosDatasource: LocalVideoDataSource());
+
     return MultiProvider(
-      providers: [ChangeNotifierProvider(
-        lazy: false,
-        create: (_) => DiscoverProvider()..loadNextPage())],
+      providers: [
+        ChangeNotifierProvider(
+            lazy: false,
+            create: (_) =>
+                DiscoverProvider(videosRepository: videoPostRepository)
+                  ..loadNextPage())
+      ],
       child: MaterialApp(
           title: 'TokTik',
           debugShowCheckedModeBanner: false,
